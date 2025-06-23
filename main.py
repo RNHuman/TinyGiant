1642db66097f4300dedadbd6125ce3529aa71a4f
Rheinhard N. Human <110828784+RNHuman@users.noreply.github.com>
2025-06-23 10:47:07 +0200

    Launch Update
    This browser runs in permanent incognito mode:
    - No history, cookies, or cache are saved
    - All session data is deleted automatically when the app closes
    - A startup warning informs the user of private mode

--- main.py
+++ main.py
@@ -1,61 +1,45 @@
 import sys
 from PyQt5.QtWidgets import (
-    QApplication, QMainWindow, QTabWidget, QWidget, QVBoxLayout, QLineEdit, QPushButton, QToolBar, QAction, QFileDialog
+    QApplication, QMainWindow, QTabWidget, QWidget, QVBoxLayout, QLineEdit, QPushButton,
+    QToolBar, QAction, QFileDialog, QMessageBox
 )
-from PyQt5.QtWebEngineWidgets import QWebEngineView, QWebEngineDownloadItem, QWebEngineProfile
+from PyQt5.QtWebEngineWidgets import QWebEngineView, QWebEngineProfile
 from PyQt5.QtCore import QUrl
+
 
 class BrowserTab(QWidget):
     def __init__(self):
         super().__init__()
-
-        # Create a QVBoxLayout for each tab
         layout = QVBoxLayout(self)
-
-        # Create a web engine view
         self.browser = QWebEngineView()
-
-        # Add the web engine view to the layout
         layout.addWidget(self.browser)
-
-        # Open Google's home page by default
         self.browser.setUrl(QUrl("http://www.google.com"))
-
-        # Track the page load event to update the tab title
         self.browser.urlChanged.connect(self.update_tab_title)
 
     def update_tab_title(self):
-        # Get the current page title
         page_title = self.browser.page().title()
-        if page_title:
-            # Update the tab title with the page title
-            index = self.parent().parent().indexOf(self)
-            self.parent().parent().setTabText(index, page_title)
-        else:
-            # If the title isn't available, set it as "#"
-            index = self.parent().parent().indexOf(self)
-            self.parent().parent().setTabText(index, "#")
+        index = self.parent().parent().indexOf(self)
+        self.parent().parent().setTabText(index, page_title if page_title else "#")
 
-    def clear_history(self):
-        # Clear the browser history for this tab
+    def clear_data(self):
+        profile = self.browser.page().profile()
+        profile.clearHttpCache()
+        profile.clearAllVisitedLinks()
+        profile.cookieStore().deleteAllCookies()
         self.browser.history().clear()
+
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-
-        # Set window title
-        self.setWindowTitle("Basic Web Browser")
-
-        # Create a QTabWidget for multiple tabs
+        self.setWindowTitle("Private Web Browser")
         self.tabs = QTabWidget()
         self.setCentralWidget(self.tabs)
 
-        # Create toolbar for the browser
         self.toolbar = QToolBar()
         self.addToolBar(self.toolbar)
 
-        # Add Back and Forward buttons
+        # Back & Forward
         back_action = QAction("Back", self)
         back_action.triggered.connect(self.go_back)
         self.toolbar.addAction(back_action)
@@ -64,92 +48,83 @@ class MainWindow(QMainWindow):
         forward_action.triggered.connect(self.go_forward)
         self.toolbar.addAction(forward_action)
 
-        # Add a new tab button to the toolbar
+        # New Tab
         new_tab_action = QAction("New Tab", self)
         new_tab_action.triggered.connect(self.add_new_tab)
         self.toolbar.addAction(new_tab_action)
 
-        # Add Print and Save as PDF button
-        print_action = QAction("Print / Save as PDF", self)
-        print_action.triggered.connect(self.print_page)
-        self.toolbar.addAction(print_action)
-
-        # Create an address bar
+        # Address bar + Go
         self.address_bar = QLineEdit()
         self.toolbar.addWidget(self.address_bar)
 
-        # Add "Go" button next to the address bar
         go_button = QPushButton("Go")
         go_button.clicked.connect(self.navigate_to_url)
         self.toolbar.addWidget(go_button)
 
-        # Add a default tab
+        # Download manager
+        QWebEngineProfile.defaultProfile().downloadRequested.connect(self.handle_download)
+
+        # Add default tab
         self.add_new_tab()
 
-        # Set up the download manager
-        profile = QWebEngineProfile.defaultProfile()
-        profile.downloadRequested.connect(self.handle_download)
+        # Show Incognito Warning
+        self.show_incognito_warning()
+
+    def show_incognito_warning(self):
+        QMessageBox.information(
+            self,
+            "Incognito Mode",
+            "You are browsing in incognito mode.\n"
+            "No history, cookies, or cache will be saved.\n"
+            "All data will be deleted when you close the browser.",
+            QMessageBox.Ok
+        )
 
     def add_new_tab(self):
-        # Create a new tab with a BrowserTab widget
         new_tab = BrowserTab()
-        index = self.tabs.addTab(new_tab, "#")  # Default tab title is "#"
+        index = self.tabs.addTab(new_tab, "#")
         self.tabs.setCurrentIndex(index)
-
-        # When a new tab is selected, update the address bar
         self.tabs.currentChanged.connect(self.update_address_bar)
 
     def navigate_to_url(self):
-        # Get the current tab
         current_tab = self.tabs.currentWidget()
-
-        # Navigate to the URL in the address bar
         url = self.address_bar.text()
         if not url.startswith("http"):
             url = "http://" + url
         current_tab.browser.setUrl(QUrl(url))
 
     def update_address_bar(self):
-        # Update the address bar with the URL of the current tab
         current_tab = self.tabs.currentWidget()
-        if current_tab is not None:
-            url = current_tab.browser.url().toString()
-            self.address_bar.setText(url)
+        if current_tab:
+            self.address_bar.setText(current_tab.browser.url().toString())
 
     def go_back(self):
-        # Navigate back in the current tab
         current_tab = self.tabs.currentWidget()
         if current_tab:
             current_tab.browser.back()
 
     def go_forward(self):
-        # Navigate forward in the current tab
         current_tab = self.tabs.currentWidget()
         if current_tab:
             current_tab.browser.forward()
 
-    def print_page(self):
-        # Print the current page or save as PDF
-        current_tab = self.tabs.currentWidget()
-        if current_tab:
-            printer = QWebEngineView()
-            file_dialog = QFileDialog.getSaveFileName(self, "Save as PDF", "", "*.pdf")
-            if file_dialog[0]:
-                current_tab.browser.page().printToPdf(file_dialog[0])
-
     def handle_download(self, download):
-        # Handle file downloads
         save_path, _ = QFileDialog.getSaveFileName(self, "Save File", download.path())
         if save_path:
             download.setPath(save_path)
             download.accept()
 
     def closeEvent(self, event):
-        # Clear history for all tabs when closing the application
+        # Full privacy cleanup
         for i in range(self.tabs.count()):
             tab = self.tabs.widget(i)
-            tab.clear_history()
+            tab.clear_data()
+        profile = QWebEngineProfile.defaultProfile()
+        profile.cookieStore().deleteAllCookies()
+        profile.clearHttpCache()
+        profile.clearAllVisitedLinks()
         event.accept()
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
